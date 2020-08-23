bb445ad0dd71d84a1c71c99e955a341c416d7bf6
hsimao <e087754958@gmail.com>
2020-08-23 17:07:54 +0800

    feat: access token route logic

--- src/app/application.go
+++ src/app/application.go
@@ -1,10 +1,21 @@
 package app
 
-import(
+import (
+	"github.com/gin-gonic/gin"
 	"github.com/hsimao/bookstore_auth-api/src/domain/access_token"
+	"github.com/hsimao/bookstore_auth-api/src/http"
 	"github.com/hsimao/bookstore_auth-api/src/repository/db"
-	)
+)
+
+var (
+	router = gin.Default()
+)
 
 func StartApplication() {
-	atService := access_token.NewService(db.NewRepository())
+	atService := access_token.NewService(db.NewRepository())
+	atHandler := http.NewHandler(atService)
+
+	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+
+	router.Run(":8080")
 }
